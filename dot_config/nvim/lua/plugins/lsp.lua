11266e7adb3cee8f540609d467463155c7ac5d61
JeTondsLeGazon <michel.hugo94@gmail.com>
2025-05-26 06:35:19 +0200

    update

---
+++ dot_config/nvim/lua/plugins/lsp.lua
@@ -0,0 +1,36 @@
+return {
+	{
+	"mason-org/mason.nvim",
+	opts = {},
+	config = function()
+	require("mason").setup()
+	end,
+	},
+	{
+    "mason-org/mason-lspconfig.nvim",
+    opts = {},
+    dependencies = {
+        { "mason-org/mason.nvim", opts = {} },
+        "neovim/nvim-lspconfig",
+	},
+    config = function()
+	require("mason-lspconfig").setup({
+		ensure_installed = {
+			"lua_ls",
+			"basedpyright",
+		}
+	})
+	end
+	},
+	{
+	"neovim/nvim-lspconfig",
+	config = function()
+		local lspconfig = require("lspconfig")
+		lspconfig.lua_ls.setup({})
+		lspconfig.basedpyright.setup({})
+		vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
+		vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
+		vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, {})
+	end,
+	}
+}
